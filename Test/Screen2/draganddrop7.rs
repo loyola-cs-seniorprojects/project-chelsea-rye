15ad6dc3759aa48698340fde79fef98c4e5a3207
Chelsea Meier <31703745+cnmeier@users.noreply.github.com>
2020-05-01 16:20:04 -0400

    Create draganddrop7.rs

---
+++ Test/Screen2/draganddrop7.rs
@@ -0,0 +1,510 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>body_Drag and drop the courses listed into _92d01c</name>
+   <tag></tag>
+   <elementGuidId>b3792299-8c70-4614-b158-55d7e6de0882</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//body</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>body</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+         Drag and drop the courses listed into each of the boxes to create a degree plan 
+
+
+
+
+
+
+
+    
+    User Manual
+
+    
+    
+        
+            
+                 
+                    How To Use The Loyola University Degree Planner
+                
+                
+                    
+		
+
+HOW TO USE THE LOYOLA UNIVERSITY DEGREE PLANNER
+1. The first screen will appear with a drop-down menu. Choose a degree/major/minor combination that you are interested in pursuing.
+2. Click the Next button to go to the next page. The second screen will appear containing a layout for you to start planning your four years. On the left hand side all the required courses for your selected degree/major/minor combination will be listed. 
+3. You can drag and drop each one into one of the 12 sections covering the rest of the page.There are 8 semester sections (one fall and one spring for each of your four years at Loyola) and 4 summer sections that follow after each school year.
+4. You may move each course as often as needed within the sections.
+5. Once you have completed your degree plan, you can click on the Print button which will allow you to print out or save your degree plan.
+6. The Back button will allow you to go back to the first screen to select a new degree/major/minor combination and start the process all over again.
+                
+                
+                    Close
+                
+            
+        
+    
+
+
+
+
+  
+  
+       
+
+
+
+
+        
+
+
+
+
+
+
+
+BS Computer Science Major, Math Minor
+
+
+
+ 
+
+ 
+   
+
+        Writing
+
+   
+
+ 
+   
+
+        Lower Level History
+
+   
+
+ 
+   
+
+        Upper Level History
+
+   
+
+ 
+   
+
+        Lower Level English
+
+   
+
+ 
+   
+
+        Upper Level English
+
+   
+
+ 
+   
+
+        Foreign Language 101
+
+   
+
+ 
+   
+
+        Foreign Language 102
+
+   
+
+ 
+   
+
+        Foreign Language 103
+
+   
+
+ 
+   
+
+        Foreign Language 104
+
+   
+
+ 
+   
+
+        Social Science Core
+
+   
+
+ 
+   
+
+        Social Science Core
+
+   
+
+ 
+   
+
+        Fine Arts
+
+   
+
+ 
+   
+
+        MA251
+
+   
+
+ 
+   
+
+        CS151
+
+   
+
+ 
+   
+
+        PH201&amp;PH291
+
+   
+
+ 
+   
+
+        Lower Level Philosophy
+
+   
+
+ 
+   
+
+        Upper Level Philosophy
+
+   
+
+ 
+   
+
+        Lower Level Theology
+
+   
+
+ 
+   
+
+        Upper Level Theology
+
+   
+
+ 
+   
+
+        Ethics
+
+   
+
+ 
+   
+
+        CS212
+
+   
+
+ 
+   
+
+        CS312
+
+   
+
+ 
+   
+
+        CS366
+
+   
+
+ 
+   
+
+        CS371
+
+   
+
+ 
+   
+
+        MA252/427/445/447/448/481/485/ST485/ST265
+
+   
+
+ 
+   
+
+        CS295/MA295/MA395
+
+   
+
+ 
+   
+
+        MA301
+
+   
+
+ 
+   
+
+        ST210
+
+   
+
+ 
+   
+
+        CS451
+
+   
+
+ 
+   
+
+        CS462
+
+   
+
+ 
+   
+
+        CS466
+
+   
+
+ 
+   
+
+        CS482
+
+   
+
+ 
+   
+
+        CS496
+
+   
+
+ 
+   
+
+        CS400+ Level Elective
+
+   
+
+ 
+   
+
+        CS400+ Level Elective
+
+   
+
+ 
+   
+
+        CS400+ Level Elective
+
+   
+
+ 
+   
+
+        BL118/BL201/CH101/PH202
+
+   
+
+ 
+   
+
+        MA400 Level Elective
+
+   
+
+ 
+   
+
+        MA/ST200+ Level Elective
+
+   
+
+ 
+   
+
+        Non-departmental Elective
+
+   
+
+ 
+   
+
+        Free Elective
+
+   
+
+ 
+   
+
+        Free Elective
+
+   
+
+ 
+   
+
+        Free Elective
+
+   
+
+ 
+   
+
+        Job/Internship
+
+   
+
+ 
+   
+
+        Job/Internship
+
+   
+
+ 
+   
+
+        Job/Internship
+
+   
+
+ 
+   
+
+        Job/Internship
+
+   
+
+ 
+
+
+
+ 
+ 
+ 
+
+
+
+        
+        
+                
+                        Freshman
+                        Sophomore
+                        Junior
+                        Senior
+                
+                
+                        Fall Semester
+                        Fall Semester
+                        Fall Semester
+                        Fall Semester
+                
+
+
+                
+                        
+                        
+                        
+                        
+                
+
+
+                 
+                        Spring Semester
+                        Spring Semester
+                        Spring Semester
+                        Spring Semester
+                
+
+                
+                        
+                        
+                        
+                        
+                
+
+
+                 
+                        Summer
+                        Summer
+                        Summer
+                        Summer
+                
+
+                
+                        
+                        
+                        
+                        
+                
+        
+
+        Writing
+
+   
+
+
+ 
+
+
+
+         
+
+
+id(&quot;table&quot;)/tbody[1]/tr[3]/td[@class=&quot;boxes&quot;]</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//body</value>
+   </webElementXpaths>
+</WebElementEntity>
